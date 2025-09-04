3cdf8fa6be9fca733f720dde9102b8700cd64a8f
bbchung <afafaf4@gmail.com>
2025-09-04 14:43:23 +0800

    update

--- lua/plugins/browsing.lua
+++ lua/plugins/browsing.lua
@@ -45,5 +45,35 @@ return {
                 end,
             })
         end
-    }
+    },
+    {
+        'chrisbra/csv.vim',
+        init = function()
+            vim.g.no_csv_maps = 1
+        end,
+        config = function()
+            -- CSV plugin settings
+            vim.g.csv_no_progress = 1
+
+            -- Start and end variables
+            vim.g.start = 1
+            vim.g["end"] = 10 -- 'end' is a Lua keyword, so use string indexing
+            -- Define the formatting function
+            function _G.CsvFormat()
+                vim.cmd("'<,'>ArrangeColumn")
+                return 0
+            end
+
+            -- Set formatexpr for CSV files
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "csv",
+                callback = function()
+                    vim.opt_local.formatexpr = "v:lua.CsvFormat()"
+                end,
+            })
+        end
+    },
+    {
+        "tpope/vim-fugitive"
+    },
 }
