a911fc4620dc38edd958ebec76715732dda548b4
bbchung <afafaf4@gmail.com>
2025-09-04 15:43:56 +0800

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,11 +6,10 @@ return {
                 opts = opts or { silent = true }
                 vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
             end
-            -- 常用 LSP 快捷鍵
             buf_map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
             buf_map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-            buf_map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
             buf_map("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
+            buf_map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
             vim.keymap.set("n", "<Leader>k", function()
                 vim.lsp.buf.format({ async = true })
             end, { noremap = true, silent = true })
@@ -37,6 +36,23 @@ return {
                 cmd = { "clangd", "--background-index", "--query-driver=/usr/bin/clang++", "--pch-storage=memory", "--clang-tidy" },
                 filetypes = { "c", "cpp" },
                 root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
+                on_attach = function(client, bufnr)
+                    -- define a buffer-local command :A
+                    vim.api.nvim_buf_create_user_command(bufnr, "A", function()
+                        local params = { uri = vim.uri_from_bufnr(0) }
+                        vim.lsp.buf_request(0, "textDocument/switchSourceHeader", params, function(err, result)
+                            if err then
+                                vim.notify("clangd switchSourceHeader error: " .. err.message, vim.log.levels.ERROR)
+                                return
+                            end
+                            if not result then
+                                vim.notify("No corresponding header/source file found", vim.log.levels.WARN)
+                                return
+                            end
+                            vim.cmd("edit " .. vim.uri_to_fname(result))
+                        end)
+                    end, { desc = "Switch between source/header (clangd)" })
+                end,
             })
             lspconfig.pyright.setup({
                 capabilities = capabilities,
