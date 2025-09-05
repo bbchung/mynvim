cf59b93f1ca01eaef2502df50df089e4a2e0ad22
bbchung <afafaf4@gmail.com>
2025-09-05 17:18:28 +0800

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,6 +2,18 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            -- Inside your on_attach = function(client, bufnr) ... end
+            vim.keymap.set({ 'n', 'v' }, '<leader>x', function()
+                vim.lsp.buf.code_action({
+                    context = {
+                        only = {
+                            "quickfix",
+                            "source.fixAll",
+                        },
+                    },
+                    apply = true,
+                })
+            end, { silent = true, desc = "LSP Quick Fix" })
             vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
             vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { silent = true })
             vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", { silent = true })
