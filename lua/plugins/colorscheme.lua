6536366a4a7465f441569f490c91306d76d0f6db
bbchung <afafaf4@gmail.com>
2025-09-19 10:01:06 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -47,12 +47,18 @@ return {
     },
     {
         "sainnhe/gruvbox-material",
+        lazy = false,
         priority = 1000,
         config = function()
-            vim.g.gruvbox_material_background = 'medium'
+            vim.g.gruvbox_material_background = 'hard'
             vim.g.gruvbox_material_enable_italic = 0
-            vim.g.gruvbox_material_disable_italic_comment = 1
-            vim.g.gruvbox_material_sign_column_background = 'none'
+            vim.g.gruvbox_material_disable_italic_comment = 0
+            vim.g.gruvbox_material_diagnostic_text_highlight = 1
+            vim.g.gruvbox_material_diagnostic_line_highlight = 1
+            vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_spell_foreground = 'colored'
+            vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
+            vim.g.gruvbox_material_current_word = 'high contrast background'
         end,
     },
     {
