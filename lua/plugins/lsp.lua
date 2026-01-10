73e11116bae5e91812803b87eacff4b70aa79424
bbchung <afafaf4@gmail.com>
2026-01-10 23:55:20 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -115,5 +115,13 @@ return {
             vim.lsp.enable("cmake")
             vim.lsp.enable("yamlls")
         end
-    }
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "neovim/nvim-lspconfig", "mason-org/mason.nvim" },
+    },
 }
