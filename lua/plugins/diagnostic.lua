5fc2506fa7888ff0d8c563691cb8bd39b96c4ff9
bbchung <afafaf4@gmail.com>
2026-01-12 11:57:53 +0800

    update

--- lua/plugins/diagnostic.lua
+++ lua/plugins/diagnostic.lua
@@ -77,5 +77,13 @@ return {
             auto_preview = false,
         },
     },
-
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy",
+        priority = 1000,
+        config = function()
+            require("tiny-inline-diagnostic").setup()
+            vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+        end,
+    }
 }
