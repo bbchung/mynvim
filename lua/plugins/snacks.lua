8be0a37e7d9cdc1b3a9a7a82a6fc70aec6db6842
bbchung <afafaf4@gmail.com>
2026-05-14 21:08:33 +0800

    refactor

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            bigfile = { enabled = true },
+            dashboard = { enabled = false },
+            explorer = { enabled = false },
+            indent = { enabled = true },
+            input = { enabled = true },
+            notifier = {
+                enabled = true,
+                timeout = 3000,
+            },
+            picker = {
+                enabled = true,
+                layout = {
+                    preview = false, -- 徹底關閉預覽
+                },
+            },
+            quickfile = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = false },
+            statuscolumn = { enabled = false },
+            words = { enabled = false },
+            styles = {
+                notification = {
+                    -- wo = { wrap = true } -- Wrap notifications
+                }
+            }
+        },
+        keys = {
+            { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+            { "<leader>f", function() Snacks.picker.files() end,   desc = "Find Files" },
+        },
+    },
+}
