b36766f0f72eb2242d8795b46e3c7ed9a5dd5d45
bbchung <afafaf4@gmail.com>
2026-06-18 15:01:55 +0800

    update

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            completions = {
+                lsp = { enabled = true },
+            },
+            html = { enabled = false },
+            latex = { enabled = false },
+        },
+    },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,65 +1,65 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
-		---@type snacks.Config
-		opts = {
-			bigfile = { enabled = false },
-			dashboard = { enabled = false },
-			explorer = { enabled = false },
-			indent = { enabled = false },
-			input = { enabled = true },
-			notifier = {
-				enabled = true,
-				timeout = 3000,
-			},
-			picker = {
-				enabled = true,
-				layout = {
-					hidden = { "preview" },
-				},
-			},
-			quickfile = { enabled = true },
-			scope = { enabled = true },
-			scroll = { enabled = false },
-			statuscolumn = { enabled = false },
-			words = { enabled = false },
-			styles = {
-				notification = {
-					-- wo = { wrap = true } -- Wrap notifications
-				},
-			},
-		},
-		keys = {
-			{
-				"<leader>b",
-				function()
-					Snacks.picker.buffers()
-				end,
-				desc = "Buffers",
-			},
-			{
-				"<leader>f",
-				function()
-					Snacks.picker.files()
-				end,
-				desc = "Find Files",
-			},
-			{
-				"<leader>s",
-				function()
-					Snacks.picker.grep()
-				end,
-				desc = "全專案關鍵字搜尋",
-			},
-			{
-				"<leader>g",
-				function()
-					Snacks.picker.grep_word()
-				end,
-				desc = "搜尋游標所在的單字",
-			},
-		},
-	},
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            bigfile = { enabled = false },
+            dashboard = { enabled = false },
+            explorer = { enabled = false },
+            indent = { enabled = false },
+            input = { enabled = true },
+            notifier = {
+                enabled = true,
+                timeout = 3000,
+            },
+            picker = {
+                enabled = true,
+                layout = {
+                    hidden = { "preview" },
+                },
+            },
+            quickfile = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = false },
+            statuscolumn = { enabled = false },
+            words = { enabled = false },
+            styles = {
+                notification = {
+                    -- wo = { wrap = true } -- Wrap notifications
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>b",
+                function()
+                    Snacks.picker.buffers()
+                end,
+                desc = "Buffers",
+            },
+            {
+                "<leader>f",
+                function()
+                    Snacks.picker.files()
+                end,
+                desc = "Find Files",
+            },
+            {
+                "<leader>S",
+                function()
+                    Snacks.picker.grep()
+                end,
+                desc = "全專案關鍵字搜尋",
+            },
+            {
+                "<leader>G",
+                function()
+                    Snacks.picker.grep_word()
+                end,
+                desc = "搜尋游標所在的單字",
+            },
+        },
+    },
 }
